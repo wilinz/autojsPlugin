16c4151b209a7d831ca6c99a74049776f8a32078
go <go@qq.com>
2022-08-31 00:55:24 +0800

    🎉init

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "autojs"
+pluginManagement {
+    repositories {
+        // 插件使用阿里云 maven 源
+        maven {
+            setUrl("https://maven.aliyun.com/repository/gradle-plugin")
+        }
+    }
+}
